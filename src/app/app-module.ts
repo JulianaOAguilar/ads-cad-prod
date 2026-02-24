aa2cb8be54f7f83b07c81b339ea6cd73a5f326dc
glaucotodesco <glauco.todesco@gmail.com>
2026-02-24 11:10:49 -0300

    feat: component product

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Product } from './product/product';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Product
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/product/product.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: false,
+  templateUrl: './product.html',
+  styleUrl: './product.css',
+})
+export class Product {
+
+   formGroupProduct: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+
+    this.formGroupProduct = formBuilder.group({
+      id: [''],
+      name: ['Glauco Todesco'],
+      description: ['FATEC'],
+      price: [0]
+    });
+
+  }
+}
